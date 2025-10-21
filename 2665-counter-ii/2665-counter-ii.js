517c9a5f4760634f81126b386330e2f7d2dfd57b
Manorath Bhatt <115536934+manibhatt10@users.noreply.github.com>
2025-10-21 13:55:06 +0545

    Time: 56 ms (32.63%), Space: 55.9 MB (88.66%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initialValue=init;
+    return{
+        increment(){
+            initialValue+=1;
+            return initialValue
+        },
+        decrement(){
+            initialValue-=1;
+            return initialValue;
+        },
+        reset(){
+            initialValue=init;
+            return initialValue;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
